056976809dc49c803eb0476dafb83a54c64e7c29
Farzaneh <farzaneh.v@gmail.com>
2020-07-14 16:54:35 +0430

    Template string example

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-var name = "R";
-alert(name);
+let fname = 'Ryan';
+let lname = 'D';
+let age = prompt('enter Ryan age ');
+
+
+
+//old way
+//let result = fname + " " + lname + " " + age + " is years old";
+//alert(result);
+//
+//
+//new way
+//Template strings
+let result = `${fname} ${lname} is ${age} years old`;
+alert(result);
